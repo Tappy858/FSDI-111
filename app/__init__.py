e92091d78b93f08a80cbc24ac216dc0a370f9b48
darius <darius_taplin@yahoo.com>
2024-03-05 21:36:22 -0800

    added prod

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "hello world"
+
+@app.route("/products")
+def products():
+    product_list = ["apples", "oranges", "bananas"]
+    bullet_list = "".join("<li>%s</li>" % product for product in product_list)
+    return "<h1>Products:</h1><ul>%s</ul>" % bullet_list
+
+
+
+
